e7909b6fb76de18ff3bf7a3c1151b6ba0d78a43c
Joe Averbukh <joeaverbukh@gmail.com>
2014-02-20 20:43:40 -0800

    Combination Locker

---
+++ easy/148_combo_lock/cl_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Author: nezaj
+# Date: 02/20/14
+
+from combo_lock import clockwise_turns, counter_clockwise_turns, get_combo_turns
+
+def is_eq(act, exp):
+    assert act == exp, "Expected {} got {}".format(exp, act)
+
+def test_clockwise():
+    # Turning clockwise from 0 to 0 should take 0 turns
+    act_turns = clockwise_turns(5, 0, 0)
+    exp_turns = 0
+    is_eq(act_turns, exp_turns)
+
+    # Turning clockwise from 1 to 3 should take 2 turns
+    act_turns = clockwise_turns(5, 1, 3)
+    exp_turns = 2
+    is_eq(act_turns, exp_turns)
+
+    # Turning clockwise from 3 to 1 should take 3 turns
+    act_turns = clockwise_turns(5, 3, 1)
+    exp_turns = 3
+    is_eq(act_turns, exp_turns)
+
+def test_counter_clockwise():
+    # Turning counter-clockwise from 0 to 0 should take 0 turns
+    act_turns = counter_clockwise_turns(5, 0, 0)
+    exp_turns = 0
+    is_eq(act_turns, exp_turns)
+
+    # Turning counter-clockwise from 1 to 2 should take 4 turns
+    act_turns = counter_clockwise_turns(5, 1, 2)
+    exp_turns = 4
+    is_eq(act_turns, exp_turns)
+
+    # Turning counter-clockwise from 2 to 1 should take 1 turn
+    act_turns = counter_clockwise_turns(5, 2, 1)
+    exp_turns = 1
+    is_eq(act_turns, exp_turns)
+
+def test_get_combo_turns():
+    params = [5, 1, 2, 3]
+
+    # Two full turns, clockwise to 1, full turn, counter-clockwise to 2,
+    # clockwise to three should take 21 turns
+    exp_turns = 21
+    act_turns = get_combo_turns(*params)
+    is_eq(act_turns, exp_turns)
+
+def run_tests():
+    test_clockwise()
+    test_counter_clockwise()
+    test_get_combo_turns()
+
+def main():
+    run_tests()
+    print "All tests pass!"
+
+if __name__ == "__main__":
+    main()
---
+++ easy/148_combo_lock/combo_lock.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# Author: nezaj
+# Date: 02/20/14
+
+# http://www.reddit.com/r/dailyprogrammer/comments/1v4cjd/011314_challenge_148_easy_combination_lock/
+# [01/13/14] Challenge #148 [Easy] Combination Lock
+
+def clean_params(params):
+    """
+    Converts a space delimited string of numbers and converts them
+    into an array of integers
+    """
+    if isinstance(params, str):
+        params = map(int, params.split(' '))
+
+    return params
+
+def clockwise_turns(n, start, stop):
+    """
+    Returns number of clockwise turns from start to stop.
+
+    If the stop is after the start the number of turns
+    is just the difference between the two.
+
+    If the stop is before the start than we need to turn
+    all the way to the end first and turn to the stopping point.
+    """
+    return (stop - start) if (stop >= start) else (n - start) + stop
+
+def counter_clockwise_turns(n, start, stop):
+    """
+    Returns number of counter-clockwise turns from start to stop.
+
+    The number of counter-clockwise turns can be thought of in terms
+    of clockwise turns. It is equivalent to the difference between
+    the number of turns in a full rotation and the number of clockwise turns
+    from start to stop
+    """
+    return 0 if start == stop else n - clockwise_turns(n, start, stop)
+
+def get_combo_turns(n, first, second, third, start=0):
+    " Returns total number of turns for locker combination "
+    turns = 2 * n
+    turns += clockwise_turns(n, start, first)
+    turns += n
+    turns += counter_clockwise_turns(n, first, second)
+    turns += clockwise_turns(n, second, third)
+
+    return turns
+
+def main():
+    params = clean_params(raw_input("Enter input: "))
+    print get_combo_turns(*params)
+
+if __name__ == "__main__":
+    main()
